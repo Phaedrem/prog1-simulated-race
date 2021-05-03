30c5eeb852cbe094111bbede3c7415ff182fbe1a
Darren Bowers <darren.bowers@g.austincc.edu>
2021-05-03 16:59:44 -0500

    created main.py and vehicles.py with comment headers and imports

---
+++ main.py
@@ -0,0 +1,25 @@
+######################
+# Name: Darren Bowers
+# Final Project
+# Purpose: Program to import .dat file, filter out incomplete data and then
+# perform a race based on random number generations. 
+######################
+
+import sys
+import os.path
+import vehicles as vh
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+main()
---
+++ vehicles.py
@@ -0,0 +1,9 @@
+######################
+# Name: Darren Bowers
+# Final Project
+# Purpose: Program to import .dat file, filter out incomplete data and then
+# perform a race based on random number generations. 
+######################
+
+import sys
+import os.path
