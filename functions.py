722381e00a2b79269d6ca5ff5f22fc3042fafcab
Darren Bowers <darren.bowers@g.austincc.edu>
2021-05-03 18:14:53 -0500

    created a functions.py with header.

---
+++ functions.py
@@ -0,0 +1,6 @@
+######################
+# Name: Darren Bowers
+# Final Project
+# Purpose: Program to import .dat file, filter out incomplete data and then
+# perform a race based on random number generations. 
+######################
