fa9ce429f2ed9c5846942ddab6062a84afcc8a80
Darren Bowers <darren.bowers@g.austincc.edu>
2021-05-03 20:10:43 -0500

    changed order of parameters to match .dat file

--- main.py
+++ main.py
@@ -11,10 +11,20 @@ import car as Car
 
 
 def main():
+    list_of_lists = []
     argc = len(sys.argv)
     if argc==2:
         if os.path.isfile(sys.argv[1]):
-            pass
+            fin = open(sys.argv[1], 'r')
+            for line in fin:
+                stripped_line = line.strip()
+                line_list = stripped_line.split(',')
+                list_of_lists.append(line_list)
+                #model = line_list[0]
+                #make = line_list[1]
+            print(list_of_lists)
+                
+            
         else:
             print("Error: you must enter one-and-only-one parameter that is a valid file name.")
     else:
