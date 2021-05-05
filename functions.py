20757da8d3922592c6784bdbeffa190804587ca6
Darren Bowers <darren.bowers@g.austincc.edu>
2021-05-05 11:11:25 -0500

    moved the bulk of main.py code to functions.py, with corrections such as removing logic error caused by myCar previously on line 30. Also combined if statements in main.py to reduce repetition.

--- functions.py
+++ functions.py
@@ -4,3 +4,25 @@
 # Purpose: Program to import .dat file, filter out incomplete data and then
 # perform a race based on random number generations. 
 ######################
+
+import sys
+import os.path
+from car import Car
+
+def make_list(file):
+    standInList = []
+    a_file = open(sys.argv[1], 'r')
+    for line in a_file:
+        stripped_line = line.strip()
+        line_list = stripped_line.split(',')
+        length = len(line_list)
+        if length >= 2 and length < 4:
+            make = line_list[0]
+            model = line_list[1]
+            if length == 3:
+                year = line_list[2]
+            else:
+                year = ''
+            standInList.append(Car(make,model,year))
+    return(standInList)
+    a_file.close()
--- main.py
+++ main.py
@@ -8,32 +8,13 @@
 import sys
 import os.path
 from car import Car
-
+import functions as fn
 
 def main():
-    standInList = []
     argc = len(sys.argv)
-    if argc==2:
-        if os.path.isfile(sys.argv[1]):
-            a_file = open(sys.argv[1], 'r')
-            for line in a_file:
-                stripped_line = line.strip()
-                line_list = stripped_line.split(',')
-                length = len(line_list)
-                if length >= 2 and length < 4:
-                    make = line_list[0]
-                    model = line_list[1]
-                    if length == 3:
-                        year = line_list[2]
-                    else:
-                        year = ''
-                myCars =[Car(make,model,year)]
-                standInList.append(myCars)
-            for car in standInList:
-                car.displayCar()
-            a_file.close()
-        else:
-            print("Error: you must enter one-and-only-one parameter that is a valid file name.")
+    if argc==2 and os.path.isfile(sys.argv[1]):
+        for i in fn.make_list(sys.argv[1]):
+            i.displayCar()
     else:
         print("Error: you must enter one-and-only-one parameter that is a valid file name.")
 
